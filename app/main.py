f1a789bc2d67b8cb1e98c18d4aff3732159b2495
Christian Martiny <christian.martiny@outlook.com>
2025-01-20 16:46:22 +0100

    sdf

--- app/main.py
+++ app/main.py
@@ -179,6 +179,12 @@ async def login_for_access_token(
     )
     return Token(access_token=access_token, token_type="bearer")
 
+
+@app.get("/status/")
+async def read_system_status(current_user: Annotated[User, Depends(get_current_user)]):
+    return {"status": "ok"}
+
+
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
